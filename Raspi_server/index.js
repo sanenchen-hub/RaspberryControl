1553c04aa26533164b40fa370c8b0152030756f4
sanenchen <sanenchen@sanenchendeMacBook-Pro.local>
2022-01-21 13:51:37 +0800

    Server Update

--- Raspi_server/index.js
+++ Raspi_server/index.js
@@ -12,38 +12,5 @@ app.get("/raspi_base_info", function (request, response) {
     response.end(result_base_info)
 })
 
-app.get("/clash_start", function (request, response) {
-    try {
-        child_process.execSync('/etc/clash/start.sh start')
-    } catch (e) {
-        response.end("502")
-    }
-    response.end("200")
-})
-
-app.get("/clash_stop", function (request, response) {
-    try {
-        child_process.execSync('/etc/clash/start.sh stop')
-    } catch (e) {
-        response.end("502")
-    }
-    response.end("200")
-})
-
-app.get("/clash_updateyaml", function (request, response) {
-    try {
-        if (request.query.url !== undefined) {
-            const value = "sed -i \"s#Url=.*#Url=\\'" + request.query.url + "\\'#g\" /etc/clash/mark"
-            child_process.execSync(value)
-            child_process.execSync('/etc/clash/start.sh updateyaml')
-        }
-        else
-            child_process.execSync('/etc/clash/start.sh updateyaml')
-    } catch (e) {
-        response.end("502")
-    }
-    response.end("200")
-})
-
 app.listen(8080)
 console.log('Server is running at http://127.0.0.1:8080/');
